4cac4d2f1744ece5e04ac1cb12f40e57d3bcdb33
PuzzlingGGG <138084889+PuzzlingGGG@users.noreply.github.com>
2024-10-21 20:55:42 -0400

    oh my fucking god please just let this work please please please please please please please please please please please please please please please please please please please please please please please please please please please please please please please

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
